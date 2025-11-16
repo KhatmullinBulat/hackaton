83e5aac98a92ac03e4ad788321fe5887d6864443
Булат <hatmullinbulat24@gmail.com>
2025-11-16 08:25:10 +0500

    tts fix

--- server/api/tts.post.ts
+++ server/api/tts.post.ts
@@ -4,33 +4,46 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
   const targetUrl = `${config.public.api.aiBase}/ai/ai/tts`;
 
-  try {
-    const body = await readBody(event);
+  const body = await readBody(event);
 
-    const fetchOptions: RequestInit = {
-      method: event.method,
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+  const fetchOptions: RequestInit = {
+    method: event.method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const pythonResponse = await fetch(targetUrl, fetchOptions);
+  const pythonResponse = await fetch(targetUrl, fetchOptions);
 
-    if (!pythonResponse.ok || !pythonResponse.body) {
-      throw new Error("Ошибка от Python бэкенда: " + pythonResponse.statusText);
-    }
-
-    setHeader(
-      event,
-      "Content-Type",
-      pythonResponse.headers.get("Content-Type") || "audio/mpeg"
-    );
-    setHeader(event, "Transfer-Encoding", "chunked");
-
-    return sendStream(event, pythonResponse.body);
-  } catch (error) {
-    console.error("Ошибка прокси:", error);
-    return { error: "Не удалось получить стрим: " + (error as Error).message };
+  if (!pythonResponse.ok || !pythonResponse.body) {
+    throw new Error("Ошибка от Python бэкенда: " + pythonResponse.statusText);
   }
+
+  setHeader(
+    event,
+    "Content-Type",
+    pythonResponse.headers.get("Content-Type") || "audio/mpeg"
+  );
+  setHeader(event, "Transfer-Encoding", "chunked");
+
+  return sendStream(event, pythonResponse.body);
+
+  // try {
+  //   if (!pythonResponse.ok || !pythonResponse.body) {
+  //     throw new Error("Ошибка от Python бэкенда: " + pythonResponse.statusText);
+  //   }
+
+  //   setHeader(
+  //     event,
+  //     "Content-Type",
+  //     pythonResponse.headers.get("Content-Type") || "audio/mpeg"
+  //   );
+  //   setHeader(event, "Transfer-Encoding", "chunked");
+
+  //   return sendStream(event, pythonResponse.body);
+  // } catch (error) {
+  //   console.error("Ошибка прокси:", error);
+  //   return { error: "Не удалось получить стрим: " + (error as Error).message };
+  // }
 });
